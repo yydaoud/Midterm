d42e1a8ed8ca5fb06168a671252de1ca3a6e65d1
Yahya <yydaoud@umich.edu>
2024-03-07 14:16:43 -0500

    ihope this builds

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 10;
-		int b = 30;
-		int c = 20;
+		int a = 100;
+		int b = 300;
+		int c = 200;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
